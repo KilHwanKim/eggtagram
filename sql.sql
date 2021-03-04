d6c64761945e5586afe7b47266e7efe49317e510
KilHwanKim <eggkimkr@gmail.com>
2021-03-04 12:54:56 +0900

    Docs sql수정
    tbl_comment 추가

--- sql.sql
+++ sql.sql
@@ -3,8 +3,9 @@ CREATE DATABASE spring default CHARACTER SET UTF8;
 
 GRANT ALL PRIVILEGES ON spring.* TO studyuser@localhost IDENTIFIED BY 'mysql';
 USE spring;
-drop table if exists tbl_follow ;
 drop table if exists tbl_like ;
+drop table if exists tbl_comment ; 
+drop table if exists tbl_follow ;
 drop table if exists tbl_file ;
 drop table if exists tbl_article ;
 drop table if exists tbl_user;
@@ -58,6 +59,17 @@ savedir varchar(100)
 );
 alter table tbl_file add foreign key (ano) references tbl_article(ano) on delete cascade;
 alter table tbl_file add foreign key (nickname) references tbl_user(nickname) on delete cascade;
+
+create table tbl_comment(
+cno int(255) auto_increment primary key,
+nickname varchar(100),
+ano int(255),
+content varchar(1000),
+regdate timestamp default now()
+);
+alter table tbl_comment add foreign key (ano) references tbl_article(ano) on delete cascade;
+alter table tbl_comment add foreign key (nickname) references tbl_user(nickname) on delete cascade;
+
 
 
 
